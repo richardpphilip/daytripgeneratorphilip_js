54711338b025d185e202ca4d02bca6f162f3377e
richardpphilip <richardpphilip1@gmail.com>
2021-03-05 11:18:46 -0800

    I completed the destination choices while loop, i tested and it works.

--- daytripgenerator.js
+++ daytripgenerator.js
@@ -1,13 +1,31 @@
 let destinationChoices = ['Heavenly Mountain', 'NorthStar Resort', 'Squaw Valley'];
 
+let restuarantChoices= ['Mcdonalds', 'Burger King', 'Wendys'];
 
-RandomNumberGenerator();
-function RandomNumberGenerator(){
+
 let randomNumber;
+
+function RandomNumberGenerator(){
 randomNumber = Math.floor((Math.random() * 3));
-numberOutput = randomNumber;
-return numberOutput;
+return randomNumber;
 }
 
+RandomNumberGenerator();
 
-console.log(destinationChoices[numberOutput]);
+
+console.log("You will be going to" + " " + destinationChoices[randomNumber]);
+let ContinueDestinationLoop = true
+let DoYouLikeThisChoice = prompt("Do you Like this choice and want to keep it?");
+
+while (ContinueDestinationLoop === true)
+{
+    if (DoYouLikeThisChoice === "yes"){
+        ContinueDestinationLoop = false;
+    }
+    else {
+        RandomNumberGenerator();
+        console.log("how about " + destinationChoices[randomNumber]);
+        ContinueDestinationLoop = true;
+        DoYouLikeThisChoice = prompt("Do you Like this choice and want to keep it?");
+    }
+}
